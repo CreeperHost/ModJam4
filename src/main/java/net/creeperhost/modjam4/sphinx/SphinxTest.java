e4f80c90d2c90c9df0fed1e2c18a64ad5d640b39
Cloudhunter <cloudhunter@btinternet.com>
2014-05-15 15:44:22 +0100

    Latest still non working.

--- src/main/java/net/creeperhost/modjam4/sphinx/SphinxTest.java
+++ src/main/java/net/creeperhost/modjam4/sphinx/SphinxTest.java
@@ -7,6 +7,11 @@ import edu.cmu.sphinx.api.LiveSpeechRecognizer;
 //import edu.cmu.sphinx.util.props.ConfigurationManager;
 import edu.cmu.sphinx.api.Configuration;
 import edu.cmu.sphinx.api.SpeechResult;
+import edu.cmu.sphinx.linguist.language.grammar.SimpleWordListGrammar;
+import edu.cmu.sphinx.result.WordResult;
+
+import java.util.Collection;
+import java.util.List;
 
 /**
  * Created by Minion 2 on 15/05/2014.
@@ -21,11 +26,23 @@ public class SphinxTest {
         Configuration configuration = new Configuration();
 
         // Set path to acoustic model.
-        configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/acoustic/wsj_8kHz");
+        configuration.setAcousticModelPath("resource:/net/creeperhost/modjam4/sphinx/en-us");
+        //configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/acoustic/wsj_8kHz");
         // Set path to dictionary.
         configuration.setDictionaryPath("resource:/edu/cmu/sphinx/models/acoustic/wsj_8kHz/dict/cmudict.0.6d");
         // Set language model.
         configuration.setLanguageModelPath("resource:/edu/cmu/sphinx/models/language/en-us.lm.dmp");
+
+        configuration.setSampleRate(8000);
+
+        //configuration.setLanguageModelPath("resource:/net/creeperhost/modjam4/sphinx");
+
+
+
+        //configuration.setGrammarPath("resource:/net/creeperhost/modjam4/sphinx");
+        //configuration.setGrammarName("hello");
+        //configuration.setUseGrammar(true);
+
         try {
             liverecog = new LiveSpeechRecognizer(configuration);
         } catch (Exception e)
@@ -35,6 +52,7 @@ public class SphinxTest {
         if(liverecog!=null) {
             liverecog.startRecognition(true);
         }
+
       // loop the recognition until the programm exits.
         Runnable runnable = new Runnable() {
             @Override
@@ -54,7 +72,22 @@ public class SphinxTest {
     }
     public static void processResult(SpeechResult output)
     {
-        System.out.println("We need to do something with output.getWords()");
+        //System.out.println(output.getHypothesis());
+        List<WordResult> list = output.getWords();
+
+        Collection<String> collection = output.getNbest(10);
+
+        for (String str : collection)
+        {
+            System.out.println(str);
+        }
+/*        for (WordResult word : list)
+        {
+            System.out.println(word.getConfidence());
+            System.out.println(word.getScore());
+            System.out.println(word.getPronunciation());
+        }*/
+        //System.out.println("We need to do something with output.getWords()");
         //Need to do something with output.getWords();
     }
 
