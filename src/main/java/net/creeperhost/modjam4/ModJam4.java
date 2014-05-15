1a65e06b1b3d5017d308f0102eb1f1236e0034be
Philip Hansen <me@philiphansen.dk>
2014-05-15 03:18:20 +0200

    AutoCommit: 2014-05-15 03:18:20. 0 deletions, 1 adds, 4 modifications

--- src/main/java/net/creeperhost/modjam4/ModJam4.java
+++ src/main/java/net/creeperhost/modjam4/ModJam4.java
@@ -1,20 +1,33 @@
 package net.creeperhost.modjam4;
 
-import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import net.creeperhost.modjam4.reference.ModInfo;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ModJam4.MODID, version = ModJam4.VERSION)
-public class ModJam4
-{
-    public static final String MODID = "modjam4";
-    public static final String VERSION = "${version}";
-    
+@Mod(modid = ModInfo.ID, name = ModInfo.NAME, version = ModInfo.VERSION)
+public class ModJam4 {
+	public static final Logger logger = LogManager.getLogger(ModInfo.NAME);
+
     @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-		// some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    @SuppressWarnings("unused")
+    public void preinit(FMLPreInitializationEvent event) {
+
     }
+
+	@EventHandler
+	@SuppressWarnings("unused")
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+	@EventHandler
+	@SuppressWarnings("unused")
+	public void postinit(FMLPostInitializationEvent event) {
+		logger.info("Welcome to your mod!");
+	}
 }
---
+++ src/main/java/net/creeperhost/modjam4/reference/ModInfo.java
@@ -0,0 +1,7 @@
+package net.creeperhost.modjam4.reference;
+
+public class ModInfo {
+	public static final String ID = "modjam4";
+	public static final String NAME = "ModJam 4";
+	public static final String VERSION = "${version}";
+}
