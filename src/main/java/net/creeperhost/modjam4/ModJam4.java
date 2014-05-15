f3b63d0ae1fd869260690d1d35e6fedda2a636c9
Philip Hansen <me@philiphansen.dk>
2014-05-15 02:58:10 +0200

    Changed to CH defaults

--- src/main/java/net/creeperhost/modjam4/ModJam4.java
+++ src/main/java/net/creeperhost/modjam4/ModJam4.java
@@ -1,15 +1,15 @@
-package com.example.examplemod;
+package net.creeperhost.modjam4;
 
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = ModJam4.MODID, version = ModJam4.VERSION)
+public class ModJam4
 {
-    public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "modjam4";
+    public static final String VERSION = "${version}";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
