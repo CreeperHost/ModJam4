ad15e5aeebfe3df8015c8d5eaea902d919ab9e11
Cloudhunter <cloudhunter@btinternet.com>
2014-05-15 17:50:11 +0100

    Something working. Clean up later.

--- src/main/java/net/creeperhost/modjam4/sphinx/SphinxTest.java
+++ src/main/java/net/creeperhost/modjam4/sphinx/SphinxTest.java
@@ -1,14 +1,6 @@
 package net.creeperhost.modjam4.sphinx;
 
-import edu.cmu.sphinx.api.LiveSpeechRecognizer;
-//import edu.cmu.sphinx.frontend.util.Microphone;
-//import edu.cmu.sphinx.recognizer.Recognizer;
-//import edu.cmu.sphinx.result.Result;
-//import edu.cmu.sphinx.util.props.ConfigurationManager;
-import edu.cmu.sphinx.api.Configuration;
-import edu.cmu.sphinx.api.SpeechResult;
-import edu.cmu.sphinx.linguist.language.grammar.SimpleWordListGrammar;
-import edu.cmu.sphinx.result.WordResult;
+
 
 import java.util.Collection;
 import java.util.List;
@@ -18,77 +10,46 @@ import java.util.List;
  */
 public class SphinxTest {
 
-    public static String configLoc = "sphinx.config.xml";
-    public static LiveSpeechRecognizer liverecog = null;
-    public static boolean firststart = true;
-    public static void test()
-    {
-        Configuration configuration = new Configuration();
+    public static void test() {
 
-        // Set path to acoustic model.
-        configuration.setAcousticModelPath("resource:/net/creeperhost/modjam4/sphinx/en-us");
-        //configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/acoustic/wsj_8kHz");
-        // Set path to dictionary.
-        configuration.setDictionaryPath("resource:/edu/cmu/sphinx/models/acoustic/wsj_8kHz/dict/cmudict.0.6d");
-        // Set language model.
-        configuration.setLanguageModelPath("resource:/edu/cmu/sphinx/models/language/en-us.lm.dmp");
+        voce.SpeechInterface.init("../lib", false, true,
+                "../grammar", "digits");
 
-        configuration.setSampleRate(8000);
+        System.out.println("This is a speech recognition test. "
+                + "Speak digits from 0-9 into the microphone. "
+                + "Speak 'quit' to quit.");
 
-        //configuration.setLanguageModelPath("resource:/net/creeperhost/modjam4/sphinx");
-
-
-
-        //configuration.setGrammarPath("resource:/net/creeperhost/modjam4/sphinx");
-        //configuration.setGrammarName("hello");
-        //configuration.setUseGrammar(true);
-
-        try {
-            liverecog = new LiveSpeechRecognizer(configuration);
-        } catch (Exception e)
-        {
-            System.out.println(e.getMessage());
-        }
-        if(liverecog!=null) {
-            liverecog.startRecognition(true);
-        }
+        boolean quit = false;
 
       // loop the recognition until the programm exits.
         Runnable runnable = new Runnable() {
             @Override
             public void run() {
                 while (true) {
-                    if(!firststart) liverecog.startRecognition(false);
-                    SpeechResult result = liverecog.getResult();
-                    processResult(result);
-                    liverecog.stopRecognition();
-                    firststart=false;
+                    // Normally, applications would do application-specific things
+                    // here.  For this sample, we'll just sleep for a little bit.
+                    try
+                    {
+                        Thread.sleep(200);
+                    }
+                    catch (InterruptedException e)
+                    {
+                    }
+
+                    while (voce.SpeechInterface.getRecognizerQueueSize() > 0)
+                    {
+                        String s = voce.SpeechInterface.popRecognizedString();
+
+
+                        System.out.println("You said: " + s);
+                        //voce.SpeechInterface.synthesize(s);
+                    }
                 }
             }
 
         };
 
         new Thread(runnable).start();
+
     }
-    public static void processResult(SpeechResult output)
-    {
-        //System.out.println(output.getHypothesis());
-        List<WordResult> list = output.getWords();
-
-        Collection<String> collection = output.getNbest(10);
-
-        for (String str : collection)
-        {
-            System.out.println(str);
-        }
-/*        for (WordResult word : list)
-        {
-            System.out.println(word.getConfidence());
-            System.out.println(word.getScore());
-            System.out.println(word.getPronunciation());
-        }*/
-        //System.out.println("We need to do something with output.getWords()");
-        //Need to do something with output.getWords();
-    }
-
 }
