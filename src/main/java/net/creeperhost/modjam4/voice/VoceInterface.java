606ad349b046578e94dcaab18587396033af1613
Cloudhunter <cloudhunter@btinternet.com>
2014-05-16 14:25:01 +0100

    Remove unnecessary reflection.

--- src/main/java/net/creeperhost/modjam4/voice/VoceInterface.java
+++ src/main/java/net/creeperhost/modjam4/voice/VoceInterface.java
@@ -3,6 +3,7 @@ package net.creeperhost.modjam4.voice;
 
 
 import cpw.mods.fml.common.ObfuscationReflectionHelper;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.settings.KeyBinding;
 
 import java.awt.Robot;
@@ -16,66 +17,7 @@ import java.util.List;
 public class VoceInterface {
     public static Thread commandprocessor = null;
 
-    public static KeyBinding forward;
-    public static KeyBinding back;
-    public static KeyBinding left;
-    public static KeyBinding right;
-    public static KeyBinding jump;
-    public static KeyBinding attack;
-    public static KeyBinding use;
-    public static KeyBinding select1;
-    public static KeyBinding select2;
-    public static KeyBinding select3;
-    public static KeyBinding select4;
-    public static KeyBinding select5;
-    public static KeyBinding select6;
-    public static KeyBinding select7;
-    public static KeyBinding select8;
-    public static KeyBinding select9;
-    public static KeyBinding select10;
-
     public static void init() {
-
-        List<KeyBinding> keys = ObfuscationReflectionHelper.getPrivateValue(KeyBinding.class, null, "keybindArray");
-
-        for (KeyBinding key : keys)
-        {
-            System.out.println(key.getKeyDescription() + " " + key.getKeyCategory() + " " + key.getKeyCode());
-            if (key.getKeyDescription().equals("key.forward"))
-                forward = key;
-            else if (key.getKeyDescription().equals("key.back"))
-                back = key;
-            else if (key.getKeyDescription().equals("key.left"))
-                left = key;
-            else if (key.getKeyDescription().equals("key.right"))
-                right = key;
-            else if (key.getKeyDescription().equals("key.jump"))
-                jump = key;
-            else if (key.getKeyDescription().equals("key.attack"))
-                attack = key;
-            else if (key.getKeyDescription().equals("key.hotbar.1"))
-                select1 = key;
-            else if (key.getKeyDescription().equals("key.hotbar.2"))
-                select2 = key;
-            else if (key.getKeyDescription().equals("key.hotbar.3"))
-                select3 = key;
-            else if (key.getKeyDescription().equals("key.hotbar.4"))
-                select4 = key;
-            else if (key.getKeyDescription().equals("key.hotbar.5"))
-                select5 = key;
-            else if (key.getKeyDescription().equals("key.hotbar.6"))
-                select6 = key;
-            else if (key.getKeyDescription().equals("key.hotbar.7"))
-                select7 = key;
-            else if (key.getKeyDescription().equals("key.hotbar.8"))
-                select8 = key;
-            else if (key.getKeyDescription().equals("key.hotbar.9"))
-                select9 = key;
-            else if (key.getKeyDescription().equals("key.hotbar.10"))
-                select10 = key;
-            else if (key.getKeyDescription().equals("key.use"))
-                use = key;
-        }
 
         voce.SpeechInterface.init("../libs", false, true,
                 "../grammar", "digits");
@@ -106,9 +48,9 @@ public class VoceInterface {
                             //Lets migrate jump to make it work while your walking!
                             if(s.contains("jump")) {
                                 try {
-                                    KeyBinding.setKeyBindState(jump.getKeyCode(), true);
+                                    KeyBinding.setKeyBindState(Minecraft.getMinecraft().gameSettings.keyBindJump.getKeyCode(), true);
                                     Thread.sleep(100);
-                                    KeyBinding.setKeyBindState(jump.getKeyCode(), false);
+                                    KeyBinding.setKeyBindState(Minecraft.getMinecraft().gameSettings.keyBindJump.getKeyCode(), false);
                                 } catch (Exception e) {
 
                                 }
